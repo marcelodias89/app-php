24075d92d0e4fc75cb9e9f19cb352c503d01dcf4
marcelodias89 <marcelodias.sistemas@gmail.com>
2023-06-13 11:42:20 -0300

    Novo projeto app-php

---
+++ src/app/curso/curso.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './curso.component.html',
+  styleUrls: ['./curso.component.css']
+})
+export class CursoComponent implements OnInit{
+
+  //Construtor
+  constructor(){ }
+
+  //inicializador
+  ngOnInit(){
+    
+  }
+}
